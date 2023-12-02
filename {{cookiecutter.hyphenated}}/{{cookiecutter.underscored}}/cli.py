335b5264cd39622b10686e0e38c347a8f1fbe51d
gurrutia <durru7@gmail.com>
2023-12-02 16:07:39 -0500

    update default cli.py

--- {{cookiecutter.hyphenated}}/{{cookiecutter.underscored}}/cli.py
+++ {{cookiecutter.hyphenated}}/{{cookiecutter.underscored}}/cli.py
@@ -1,21 +1,19 @@
+from typing import Optional
+
 import click
 
 
 @click.group()
 @click.version_option()
 def cli():
+    # """Your app description goes here."""
     "{{ cookiecutter.description }}"
 
 
-@cli.command(name="command")
-@click.argument(
-    "example"
-)
-@click.option(
-    "-o",
-    "--option",
-    help="An example option",
-)
-def first_command(example, option):
-    "Command description goes here"
-    click.echo("Here is some output")
+@cli.command("example")
+@click.argument("required_argument")
+@click.option("-o", "--option", help="An option for the 'example' command.")
+def command(required_argument: str, option: Optional[str] = None):
+    """Description for the 'example' command goes here."""
+    click.echo(f"Received argument: {required_argument}")
+    click.echo(f"Received option: {option}")
